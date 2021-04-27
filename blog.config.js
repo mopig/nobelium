099d798b56af6186fb5bca2b9fd42d8124123c99
Leo <leoaee@gmail.com>
2021-04-27 23:11:24 +0800

    feat: 更新背景色

--- blog.config.js
+++ blog.config.js
@@ -7,8 +7,8 @@ const BLOG = {
   lang: 'zh-CN',
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#111827', // use hex value, don't forget '#'
+  lightBackground: '#FFFFFF', // use hex value, don't forget '#' e.g #fffefc
+  darkBackground: '#22272E', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // if leave this empty, current year will be used.
   postsPerPage: 7,
